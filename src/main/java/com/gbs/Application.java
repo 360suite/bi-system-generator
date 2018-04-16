163e365e275ee32de9155255ad3145f524205101
bobman38 <julienbras@sidoine.org>
2018-04-16 14:19:24 -0400

    better version use the right obj

--- src/main/java/com/gbs/Application.java
+++ src/main/java/com/gbs/Application.java
@@ -1,5 +1,6 @@
 package com.gbs;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import com.gbs.model.Document;
@@ -11,51 +12,74 @@ import com.google.gson.GsonBuilder;
 import net.andreinc.mockneat.MockNeat;
 
 public class Application {
+	private static int SOURCE_COUNT = 50;
+	private static int SOURCEOBJ_MIN = 20;
+	private static int SOURCEOBJ_MAX = 40;
+	private static int REPORT_COUNT = 1000;
+	private static int REPORT_SOURCE_MIN = 1;
+	private static int REPORT_SOURCE_MAX = 4;
+	private static int REPORT_SOURCEOBJ_MIN = 5;
+	private static int REPORT_SOURCEOBJ_MAX = 10;
+	private static String[] docType = {"Web Intelligence", "Desktop Intelligence", "Xcelsius" };
+
 
 	public static void main(String[] args) {
 		MockNeat mockNeat = MockNeat.threadLocal();
 		Gson gson = new GsonBuilder()
-		                        .setPrettyPrinting()
-		                        .create();
+				.setPrettyPrinting()
+				.create();
 
-		String[] docType = {"Web Intelligence", "Desktop Intelligence", "Xcelsius" };
-		
-		Set<Source> sources = mockNeat
-                .reflect(Source.class)
-                .field("internalId", mockNeat.uuids())
-                .field("name", mockNeat.regex("universe-[a-z]{2}"))
-                .field("internalId", mockNeat.uuids())
-                .field("sourceObjects",
-                           mockNeat.reflect(SourceObject.class)
-                                   .field("internalId", mockNeat.uuids())
-                                   .field("name", mockNeat.regex("universeObject\\d{5}"))
-                                   .set(10))
-                .set(100)
-                .val();
-		
 
+
+		Set<Source> sources = new HashSet<>();
+		for (int i = 0; i < SOURCE_COUNT; i++) {
+			Source source = mockNeat
+					.reflect(Source.class)
+					.field("internalId", mockNeat.uuids())
+					.field("name", mockNeat.regex("universe-[a-z]{2}"))
+					.field("internalId", mockNeat.uuids())
+					.field("sourceObjects",
+							mockNeat.reflect(SourceObject.class)
+							.field("internalId", mockNeat.uuids())
+							.field("name", mockNeat.regex("universeObject\\d{4}"))
+							.set(mockNeat.ints().range(SOURCEOBJ_MIN, SOURCEOBJ_MAX).val()))
+					.val();
+			sources.add(source);
+		}
 		System.out.println(gson.toJson(sources));
 		
-		Set<Document> docs = mockNeat
-                .reflect(Document.class)
-                .field("name", mockNeat.words())
-                .field("type", mockNeat.from(docType))
-                .field("internalId", mockNeat.uuids())
-                .field("sourceObjects",
-                           mockNeat.reflect(SourceObject.class)
-                                   .field("internalId", mockNeat.uuids())
-                                   .field("name", mockNeat.regex("universeObject\\d{5}"))
-                                   .set(10))
-                .field("source",
-                        mockNeat.reflect(Source.class)
-                                .field("internalId", mockNeat.uuids())
-                                .field("name", mockNeat.regex("universe-[a-z]{2}"))
-                                .set(2))
-                .set(10000)
-                .val();
 		
+		Set<Document> documents = new HashSet<>();
+		for (int i = 0; i < REPORT_COUNT; i++) {
+			int sourceCount = mockNeat.ints().range(REPORT_SOURCE_MIN,REPORT_SOURCE_MAX).val();
+			//System.out.println("sources count: " + sourceCount);
 
-		System.out.println(gson.toJson(docs));
+			Document doc = mockNeat
+					.reflect(Document.class)
+					.field("name", mockNeat.words())
+					.field("type", mockNeat.from(docType))
+					.field("internalId", mockNeat.uuids())
+					.val();
+			
+			doc.setSource(new HashSet<>());
+			doc.setSourceObjects(new HashSet<>());
 
+			for (int j = 0; j < sourceCount; j++) {
+				int sourceObjCount = mockNeat.ints().range(REPORT_SOURCEOBJ_MIN,REPORT_SOURCEOBJ_MAX).val();
+				Source src = mockNeat.from(sources.toArray(new Source[0])).val();
+				doc.getSource().add(mockNeat.reflect(Source.class)
+						.field("internalId", src.getInternalId())
+						.field("name", src.getName())
+						.val());
+
+				doc.getSourceObjects().addAll(mockNeat
+						.from(src.getSourceObjects().toArray(new SourceObject[0]))
+						.set(sourceObjCount)
+						.val());
+			}
+			documents.add(doc);
+		}
+		
+		System.out.println(gson.toJson(documents));
 	}
 }
--- src/main/java/com/gbs/model/Document.java
+++ src/main/java/com/gbs/model/Document.java
@@ -1,18 +1,23 @@
 package com.gbs.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Document {  
-    String name;
+    
+	String name;
     String type;
     String internalId;
     Set<SourceObject> sourceObjects;
     Set<Source> source;
+    
+    public Document() {
+    	sourceObjects = new HashSet<>();
+    	source = new HashSet<>();
+	}
 }
